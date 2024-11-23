f0c8b5ca5033d6e994a40192a7612d397a555268
chawnima <mferdyalviansyah@gmail.com>
2024-11-23 09:15:10 +0700

    add ui

--- AirJourney/src/routes/__root.jsx
+++ AirJourney/src/routes/__root.jsx
@@ -33,8 +33,8 @@ export const Route = createRootRoute({
                 </Container>
             </Navbar>
             <Container>
-                <Outlet />
             </Container>
+            <Outlet />
             <TanStackRouterDevtools />
         </>
     ),
