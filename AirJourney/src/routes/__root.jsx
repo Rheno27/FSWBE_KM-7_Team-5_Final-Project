a51a3f07c5719e88e699746c6f2e2447b71f85ec
OneR7 <rhenojulius@gmail.com>
2024-11-24 19:00:29 +0700

    add login page

--- AirJourney/src/routes/__root.jsx
+++ AirJourney/src/routes/__root.jsx
@@ -6,35 +6,7 @@ import Navbar from 'react-bootstrap/Navbar';
 export const Route = createRootRoute({
     component: () => (
         <>
-            <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-                <Container>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/">
-                            Home
-                        </Nav.Link>
-                    </Nav>
-                        <Nav>
-                            <Nav.Link as={Link} to="/register">
-                                Register
-                            </Nav.Link>
-                            <Nav.Link as={Link} to="/login">
-                            Login
-                            </Nav.Link>
-                            <Nav.Link as={Link} to="/forgot-password">
-                            forgot-password
-                            </Nav.Link>
-                            <Nav.Link as={Link} to="/otp">
-                            otp
-                            </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-            <Container>
-                <Outlet />
-            </Container>
+            <Outlet />
             <TanStackRouterDevtools />
         </>
     ),
