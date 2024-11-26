afc74887ec702bbbb3f6ce25ce6f9eabbfd4fa52
Jovina Gustari Juwita <71329253+jovinagustari@users.noreply.github.com>
2024-11-26 18:28:28 +0700

    Feature reset password page (#12)
    * add reset-password ui
    
    * adjust reset-password page ui
    
    * add & gitignore .env
    
    * add:reset-password-request

--- AirJourney/src/routes/__root.jsx
+++ AirJourney/src/routes/__root.jsx
@@ -9,6 +9,44 @@ export const Route = createRootRoute({
 });
 
 function RootComponent() {
+    const location = useLocation();
+
+    const hideNavbarRoutes = ["/register", "/login", "/reset-password", "/otp", "/reset-request"];
+
+
+    const shouldShowNavbar = !hideNavbarRoutes.includes(location.pathname);
+
+    return (    
+        <>
+            {shouldShowNavbar && (
+            <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/">
+                            Home
+                        </Nav.Link>
+                    </Nav>
+                        <Nav>
+                            <Nav.Link as={Link} to="/register">
+                                Register
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/login">
+                            Login
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/reset-password">
+                            forgot-password
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/otp">
+                            otp
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        )}
+        
     return (    
         <>
             <NavigationBar />
