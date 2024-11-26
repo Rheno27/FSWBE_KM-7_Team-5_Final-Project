fcf40ef3b0bc83fabbeac63799f69e87b063ba94
OneR7 <rhenojulius@gmail.com>
2024-11-26 20:19:29 +0700

    fix bug

--- AirJourney/src/routes/__root.jsx
+++ AirJourney/src/routes/__root.jsx
@@ -1,5 +1,9 @@
 import * as React from 'react'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
+import NavigationBar from '../components/Navbar'
+import { Container } from 'react-bootstrap'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+
 
 export const Route = createRootRoute({
     component: RootComponent,
