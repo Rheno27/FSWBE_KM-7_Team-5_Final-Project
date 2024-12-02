dae366108d0ecaf0df8d3e50275b4e5c6d11621f
Rheno Julius Andreas <117615767+Rheno27@users.noreply.github.com>
2024-12-03 00:39:14 +0700

    Feature private checkout (#26)
    * slicing page checkout pemesanan

--- AirJourney/src/components/FlightDetails/index.jsx
+++ AirJourney/src/components/FlightDetails/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, Row, Col } from 'react-bootstrap';
-import Thumbnail from '../../assets/img/thumbnail.png';
+import Thumbnail from '../../assets/img/Thumbnail.png';
 
 function FlightDetails() {
     return (
