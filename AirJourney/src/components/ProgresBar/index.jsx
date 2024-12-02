c4bf930182203158c9241124b05f801d1d25771d
Rheno Julius Andreas <117615767+Rheno27@users.noreply.github.com>
2024-12-03 00:31:30 +0700

    Feature private checkout (#25)
    * add login page
    
    * add folder service auth
    
    * add navbar user publick
    
    * add data dummy
    
    * tes deploy
    
    * tes deploy
    
    * fix error
    
    * fix bug
    
    * fix bug url
    
    * fix bug url
    
    * save progres
    
    * slicing page checkout pemesanan

--- AirJourney/src/components/ProgresBar/index.jsx
+++ AirJourney/src/components/ProgresBar/index.jsx
@@ -1,24 +1,101 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { useLocation } from '@tanstack/react-router'; 
+
 
 const ProgressBar = ({ children }) => {
+    const location = useLocation();
+    const [activeStep, setActiveStep] = useState(1); 
+
+    useEffect(() => {
+        if (location.pathname === '/users/private/checkout/') {
+            setActiveStep(1);
+        } else if (location.pathname === '/users/private/checkout/payment') {
+            setActiveStep(2);
+        } else if (location.pathname === '/users/private/checkout/success') {
+            setActiveStep(3);
+        }
+    }, [location.pathname]); 
+
     return (
-        <div className="progress-bar bg-white py-3 shadow-sm">
-            <div 
-                className="d-flex justify-content-between px-4"
-                style={{
-                    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)", 
-                    margin: "0", 
-                    padding: "0", 
-                    position: "relative", 
-                    top: "0", 
-                    width: "100%", 
-                    flexDirection: "column",
-                    alignItems: "center", 
-                }}
-            >
-                {children}
+        <Container
+            fluid
+            className="pt-4 mb-4"
+            style={{
+                boxShadow: "0px 10px 10px rgba(0, 0, 0, 0.1)",
+                paddingLeft: "0",
+                paddingRight: "0"
+            }}
+        >
+            <div className="progress-bar">
+                <div className="d-flex flex-column ">
+                    <div className="progress-steps mb-2 d-flex flex-collum align-items-start mx-4 ">
+                        <h5
+                            className="progress-step"
+                            style={{
+                                color: activeStep === 1 ? "bold" : "gray",
+                                fontWeight: activeStep === 1 ? "bold" : "normal", 
+                                marginRight: "10px",
+                            }}
+                        >
+                            Isi Data Diri
+                        </h5>
+                        &gt;
+                        <h5 
+                            className="progress-step"
+                            style={{
+                                color: activeStep === 2 ? "bold" : "gray",
+                                fontWeight: activeStep === 2 ? "bold" : "normal", 
+                                marginRight: "10px",
+                                marginLeft: "10px",
+                            }}
+                        >
+                            Bayar
+                        </h5> 
+                        &gt; 
+                        <h5 
+                            className="progress-step"
+                            style={{
+                                color: activeStep === 3 ? "bold" : "gray",
+                                fontWeight: activeStep === 3 ? "bold" : "normal", 
+                                marginLeft: "10px",
+                                marginRight: "10px",
+                            }}
+                        >
+                            Selesai
+                        </h5>
+                    </div>
+                    <Row>
+                        <Col lg={12}>
+                            <div
+                                className="timer"
+                                style={{
+                                    backgroundColor: "#FF0000",
+                                    marginTop: "10px",
+                                    borderRadius: "12px",
+                                    padding: "10px 20px",
+                                    color: "white",
+                                    marginBottom: "20px",
+                                    textAlign: "center",
+                                    position: "relative",
+                                    marginLeft: "20px",
+                                    marginRight: "20px",
+                                }}
+                            >
+                                <div
+                                    className="d-flex justify-content-center align-items-center"
+                                    style={{
+                                        width: "100%", // Memastikan elemen di dalam timer ikut penuh
+                                    }}
+                                >
+                                    Selesaikan dalam 00:15:00
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
             </div>
-        </div>
+        </Container>
     );
 };
 
