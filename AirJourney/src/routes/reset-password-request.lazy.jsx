5fb0ce5994dfc95764e6f419971bc67d831e14dc
Jovina <jovinagustari17@gmail.com>
2024-12-03 15:15:18 +0700

    fix : reset password

--- AirJourney/src/routes/reset-password-request.lazy.jsx
+++ AirJourney/src/routes/reset-password-request.lazy.jsx
@@ -1,27 +1,32 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import { useMutation } from '@tanstack/react-query'
-import { ToastContainer, toast } from "react-toastify";
-import { createLazyFileRoute, Link } from '@tanstack/react-router'
-import { Row, Col, Container, Form, Button } from 'react-bootstrap'
-import tiketkuImage from '../assets/img/tiketku.png'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useMutation } from '@tanstack/react-query';
+import { ToastContainer, toast } from 'react-toastify';
+import { Row, Col, Form, Button } from 'react-bootstrap';
+import { createLazyFileRoute, Link } from '@tanstack/react-router';
+import tiketkuImage from '../assets/img/tiketku.png';
 
 export const Route = createLazyFileRoute('/reset-password-request')({
   component: ResetRequest,
 })
 
 function ResetRequest() {
+  // Form input state
   const [email, setEmail] = useState('');
 
   let url = `${import.meta.env.VITE_API_URL}/auth/reset-password/request`;
 
-  const mutation = useMutation({
-    mutationFn: (email) => 
-      axios.post(url, { email }, { headers: { 'Content-Type': 'application/json' }}),
+  // Mutation for sending request link through email
+  const { mutate: sendRequest, isPending } = useMutation({
+    mutationFn: async (email) => 
+      await axios.post(
+        url, 
+        { email }, 
+        { headers: { 'Content-Type': 'application/json' }}
+      ),
 
     onSuccess: (response) => {
-      console.log('Response Message:', response.data?.message);
-      toast.success(response.data?.message || 'Reset link was sent successfully!.', {
+      toast.success('Reset link was sent successfully! Please check your email', {
         autoClose: 3000, 
       });
     },
@@ -31,111 +36,141 @@ function ResetRequest() {
         toast.error('Your email was not found in our records.', { 
           autoClose: 3000,
         });
-      } 
+      } else {
+        toast.error('An unexpected error occured')
+      }
     },
   })
+  // End of mutation
 
+  // Form submission handler
   const handleSubmit = async (e) => {
     e.preventDefault()
-    mutation.mutate(email)
-    console.log('submit')
+    sendRequest(email)
   }
   
   return (
-    <>
-      <Row style={{ overflow: 'hidden', height: '100vh', width: '100vw' }}>
+    <section style={{ height: "100vh", backgroundColor: "white" }}>
+      <Row className="h-100 mx-auto gap-0">
+        
+        {/* Left column with image */}
         <Col
-          md={6}
-          style={{ overflow: 'hidden', height: '100vh', position: 'relative' }}
-          className="d-none d-md-block"
+            lg={6}
+            md={12}
+            className="d-none d-lg-block p-0"
+            style={{
+                position: "relative",
+                overflow: "hidden",
+            }}
         >
           <img
-            src={tiketkuImage}
-            alt="Tiketku"
-            style={{
-              maxWidth: '100%',
-              height: 'auto',
-              objectFit: 'contain',
-              position: 'absolute',
-            }}
+              src={tiketkuImage}
+              alt="tiketku"
+              style={{
+                  width: "100%",
+                  height: "100vh",
+                  objectFit: "cover",
+              }}
           />
         </Col>
-        <Col md={6} style={{position:'relative'}}>
-            <Container
-              className="p-5 d-flex justify-content-center align-items-center"
-              style={{ minHeight: '100vh' }}
-            >
-              <div className="w-100 m-lg-5 m-0">
-                <h4 className="mb-4 fw-bold">Lupa Password</h4>
-                <Form onSubmit={handleSubmit}>
-                <ToastContainer
-                  position="bottom-center"
+        {/* End of left column */}
+
+        {/* Right column with form */}
+        <Col
+            lg={6}
+            md={12}
+            style={{position:'relative'}}
+            className="d-flex flex-column align-items-center justify-content-center"
+        >
+          <Form
+              style={{
+                  width: "100%",
+                  maxWidth: "452px",
+                  padding: "20px",
+              }}
+              onSubmit={handleSubmit}
+          >
+            {/* ToastContainer for response message */}
+            <ToastContainer
+              position="bottom-center"
+              style={{
+                position: 'absolute', 
+                bottom: 10,          
+                right: '10%',          
+                zIndex: 9999       
+              }}
+            />
+            {/* End of toast */}
+
+              <h1
+                  className="mb-4"
                   style={{
-                    position: 'absolute', 
-                    bottom: 10,          
-                    right: '10%',          
-                    zIndex: 9999       
+                      fontSize: "2rem",
+                      fontWeight: "bold",
+                      fontFamily: "Poppins, sans-serif",
+                      textAlign: "left",
+                      marginBottom: "1rem",
                   }}
-                />
-                  <Form.Group as={Col} className="mb-3" controlId="email">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control
+              >
+                  Lupa Password
+              </h1>
+
+              {/* Email form field */}
+              <Form.Group controlId="email" className="mb-4">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                      name="email"
                       type="email"
                       placeholder="Contoh: johndoe@gmail.com"
-                      style={{
-                        fontSize: '14px',
-                        lineHeight: '2',
-                        borderRadius: '15px',
-                      }}
-                      required
                       value={email}
                       onChange={(e) => setEmail(e.target.value)}
-                    />
-                  </Form.Group>
-                  <Row>
-                    <Col md={6}>
-                      <div className="d-grid">
-                        <Button
-                          as={Link}
-                          href={`/login`}
-                          style={{
-                            backgroundColor: 'white',
-                            borderColor: '#7126B5',
-                            borderRadius: '15px',
-                            lineHeight: '1.7',
-                            color: '#7126B5',
-                            marginTop: '0.8rem',
-                            boxShadow: '2px 2px 5px 1px rgba(0, 0, 0, 0.1)',
-                          }}
-                        >
-                          Kembali ke Login
-                        </Button>
-                      </div>
-                    </Col>
-                    <Col md={6}>
-                      <div className="d-grid">
-                        <Button
-                          type="submit"
-                          onClick={() => mutation.mutate()}
-                          style={{
-                            backgroundColor: '#7126B5',
-                            borderColor: '#7126B5',
-                            borderRadius: '15px',
-                            lineHeight: '1.7',
-                            marginTop: '0.8rem',
-                            boxShadow: '4px 4px 10px 2px rgba(0, 0, 0, 0.2)',
-                          }}
-                        >
-                          Kirim Permintaan
-                        </Button>
-                      </div>
-                    </Col>
-                  </Row>
-                </Form>
-              </div>
-            </Container>
+                      style={{
+                        borderRadius: "16px",
+                        marginTop: "4px"
+                      }}
+                      required
+                  />
+              </Form.Group>
+
+              {/* Buttons */}
+              <Row>
+                <Col md={6}>
+                  <div className="d-grid">
+                    <Button
+                      as={Link}
+                      href={`/login`}
+                      style={{
+                        backgroundColor: 'white',
+                        borderColor: '#7126B5',
+                        borderRadius: '16px',
+                        color: '#7126B5',
+                        boxShadow: '2px 2px 5px 1px rgba(0, 0, 0, 0.1)',
+                      }}
+                    >
+                      Kembali ke Login
+                    </Button>
+                  </div>
+                </Col>
+                <Col md={6}>
+                  <div className="d-grid">
+                    <Button
+                      type="submit"
+                      disabled={isPending}
+                      style={{
+                        backgroundColor: '#7126B5',
+                        borderColor: '#7126B5',
+                        borderRadius: '16px',
+                        boxShadow: '4px 4px 10px 2px rgba(0, 0, 0, 0.2)',
+                      }}
+                    >
+                      {isPending ? "Mengirim..." : "Kirim Permintaan"}
+                    </Button>
+                  </div>
+                </Col>
+              </Row>
+          </Form>
         </Col>
       </Row>
-    </>
+    </section>
   )
 }
--- AirJourney/src/routes/reset-password.lazy.jsx
+++ AirJourney/src/routes/reset-password.lazy.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
-import { ToastContainer, toast } from "react-toastify";
+import { useMutation } from '@tanstack/react-query';
 import { useSearchParams } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import { Row, Col, Form, Button, InputGroup } from 'react-bootstrap';
 import { createLazyFileRoute, useNavigate, Link } from '@tanstack/react-router'
-import { Row, Col, Container, Form, Button, InputGroup } from 'react-bootstrap';
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import tiketkuImage from '../assets/img/tiketku.png';
 
 export const Route = createLazyFileRoute('/reset-password')({
@@ -14,26 +16,36 @@ export const Route = createLazyFileRoute('/reset-password')({
 function ResetPassword() {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
+
+  // Get token from url
   const token = searchParams.get("token");
 
   if (!token) {
-    toast.warning("Token is missing or invalid.", {
+    toast.error("Token is missing. Please try new request", {
       position: "top-center",
     });
     return;
   }
 
+  // Input fields state
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState(''); 
+
+  // Password validation state
+  const [passwordValid, setPasswordValid] = useState(true);
+  const [matchPasswords, setMatchPasswords] = useState(true);
+
+  // Password visibility option state
   const [showPassword, setShowPassword] = useState(false);
+
   const [isTokenValid, setIsTokenValid] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [message, setMessage] = useState("");
-
+ 
   let validationUrl = `${import.meta.env.VITE_API_URL}/auth/reset-password/validate/${token}`;
   let url = `${import.meta.env.VITE_API_URL}/auth/reset-password`;
 
-  // Validate token
+  // Token validation
   useEffect(() => {
     const validateToken = async () => {
       try {
@@ -42,10 +54,7 @@ function ResetPassword() {
 
         if (response.status === 200) {
           setIsTokenValid(true);
-        } else {
-          setIsTokenValid(false);
-          toast.error("Token has expired or is invalid");
-        }
+        } 
 
       } catch (error) {
         setIsTokenValid(false);
@@ -64,153 +73,248 @@ function ResetPassword() {
     setShowPassword(!showPassword);
   }
 
+   // Update password mutation
+   const { mutate: savePassword, isPending } = useMutation({
+    mutationFn: async (email) => 
+      await axios.post(
+        url, 
+        { token, newPassword }, 
+        { headers: { 'Content-Type': 'application/json' }}
+      ),
+
+    onSuccess: (response) => {
+      toast.success('Password reset successfull. Redirecting to login page...', {
+        autoClose: 4000, 
+      });
+      setTimeout(() => navigate({ to: "/login" }), 4000);
+    },
+
+    onError: (error) => {
+      if (error.response?.status === 404) {
+        toast.error('Your email was not found in our records.', { 
+          autoClose: 3000,
+        });
+      } else {
+        toast.error(error.response?.data?.message || "An error occurred.")
+      }
+    },
+  })
+
+  // Form submission handler
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (!isTokenValid) {
-      toast.error("The token is invalid or expired.");
+      toast.error("The token has expired or is invalid.");
       return; 
     }
   
-    // Validate new password and confirm password match
-    if (newPassword !== confirmPassword) {
-      toast.warning("Passwords do not match.");
+    // Basic password validation checks
+    const validatePassword = () => {
+      if (newPassword.length < 8 || !/\d/.test(newPassword)) {
+        return "Password must be at least 8 characters long and a number.";
+      }
+  
+      if (newPassword !== confirmPassword) {
+        return "Passwords do not match.";
+      }
+  
+      return null;
+    }
+  
+    const passwordError = validatePassword();
+    if (passwordError) {
+      toast.error(passwordError);
       return;
     }
-  
-    try {
-      // Send both the token and the newPassword in the request body
-      const response = await axios.post(url, {
-        token,        
-        newPassword,  
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      toast.success('Password reset successfully. Redirecting to login...');
-      setTimeout(() => navigate({ to: "/login" }), 3000);
 
-    } catch (error) {
-      const errorMessage = error.response?.data?.message || "An error occurred.";
-      toast.error(errorMessage); 
-    }
+    // If password is valid, proceed with form submission
+    setPasswordValid(true)
+    setMessage("");
+    savePassword(token, newPassword)
   };  
 
   return (
-    <>
-      <Row style={{ overflow: 'hidden', height: '100vh', width: '100vw' }}>
+    <section style={{ height: "100vh", backgroundColor: "white" }}>
+      <Row className="h-100 mx-auto gap-0">
+        
+        {/* Left column with image */}
         <Col
-          md={6}
-          style={{ overflow: 'hidden', height: '100vh', position: 'relative' }}
-          className="d-none d-md-block"
+            lg={6}
+            md={12}
+            className="d-none d-lg-block p-0"
+            style={{
+                position: "relative",
+                overflow: "hidden",
+            }}
         >
           <img
-            src={tiketkuImage}
-            alt="Tiketku"
-            style={{
-              maxWidth: '100%',
-              height: 'auto',
-              objectFit: 'contain',
-              position: 'absolute',
-            }}
+              src={tiketkuImage}
+              alt="tiketku"
+              style={{
+                  width: "100%",
+                  height: "100vh",
+                  objectFit: "cover",
+              }}
           />
         </Col>
-        <Col md={6} className={{position: 'relative'}}>
-          <Container
-            className="p-5 d-flex justify-content-center align-items-center"
-            style={{ minHeight: '100vh' }}
-          >
-            {isLoading ? (
-                <p>Validating token, please wait...</p>
-              ) : (
-            <div className="w-100 m-lg-5 m-0">
-              <h4 className="mb-3 fw-bold">Reset Password</h4>
-                <Form onSubmit={handleSubmit}>
-                  <ToastContainer
-                    position="bottom-center"
-                    style={{
-                      position: 'absolute', 
-                      bottom: 10,          
-                      right: '10%',          
-                      zIndex: 9999       
-                    }}
-                  />
-                  <Form.Group as={Col} className="mb-3" controlId="newPassword">
-                    <Form.Label>Masukkan password baru</Form.Label>
-                    <InputGroup>
-                      <Form.Control
-                        type={showPassword ? 'text' : 'password'}
-                        placeholder="Masukkan password baru"
-                        style={{ fontSize: '14px', lineHeight: '2' }}
-                        required
-                        value={newPassword}
-                        onChange={(e) => setNewPassword(e.target.value)}
-                      />
-                      <InputGroup.Text onClick={togglePassword} style={{ cursor: 'pointer' }}>
-                        {showPassword ? <FaEyeSlash /> : <FaEye />}
-                      </InputGroup.Text>
-                    </InputGroup>
-                  </Form.Group>
-                  <Form.Group as={Col} className="mb-2" controlId="confirmPassword">
-                    <Form.Label>Ulangi password baru</Form.Label>
-                    <InputGroup>
-                      <Form.Control
-                        type={showPassword ? 'text' : 'password'}
-                        placeholder="Ulangi password baru"
-                        style={{ fontSize: '14px', lineHeight: '2' }}
-                        required
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                      />
-                      <InputGroup.Text onClick={togglePassword} style={{ cursor: 'pointer' }}>
-                        {showPassword ? <FaEyeSlash /> : <FaEye />}
-                      </InputGroup.Text>
-                    </InputGroup>
-                  </Form.Group>
-                  <div className="d-grid gap-2">
-                    <Button
-                      type="submit"
-                      style={{
-                        backgroundColor: isTokenValid ? '#7126B5' : '#bfa0d7',
-                        borderColor: isTokenValid ? '#7126B5' : '#bfa0d7',
-                        borderRadius: '10px',
-                        marginTop: '1rem',
-                        lineHeight: '1.7',
-                        boxShadow: '4px 4px 10px 2px rgba(0, 0, 0, 0.2)',
-                      }}
-                    >
-                      Simpan
-                    </Button>
-                  </div>
+        {/* End of left column */}
 
-                  <div className="text-center mt-3">
-                    <span>
-                      Sudah ingat password?{" "}
-                      <a
-                        href={`/login`}
-                        style={{ color: "#7126B5", fontWeight: "bold" }}
-                      >
-                        Masuk sekarang
-                      </a>
-                    </span>
+        {/* Right column with form */}
+        <Col
+            lg={6}
+            md={12}
+            style={{position:'relative'}}
+            className="d-flex flex-column align-items-center justify-content-center"
+        >
+        {isLoading ? (
+          <p>Validating token, please wait...</p>
+        ) : (
+          <Form
+              style={{
+                  width: "100%",
+                  maxWidth: "452px",
+                  padding: "20px",
+              }}
+              onSubmit={handleSubmit}
+          >
+            {/* ToastContainer for response message */}
+            <ToastContainer
+              position="bottom-center"
+              style={{
+                position: 'absolute', 
+                bottom: 10,          
+                right: '10%',          
+                zIndex: 9999       
+              }}
+            />
+            {/* End of toast */}
+
+              <h1
+                  className="mb-4"
+                  style={{
+                      fontSize: "2rem",
+                      fontWeight: "bold",
+                      fontFamily: "Poppins, sans-serif",
+                      textAlign: "left",
+                      marginBottom: "1rem",
+                  }}
+              >
+                  Reset Password
+              </h1>
+
+              {/* New password form field */}
+              <Form.Group controlId="newPassword" className="mb-3">
+                <Form.Label>Masukkan Password Baru</Form.Label>
+                <div style={{ position: "relative" }}>
+                  <Form.Control
+                    name='newPassword'
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder="Masukkan password baru"
+                    value={newPassword}
+                    onChange={(e) => setNewPassword(e.target.value)}
+                    style={{ 
+                      borderRadius: "16px",
+                      marginTop: "4px" 
+                    }}
+                    required
+                  />
+                  <div
+                      style={{
+                          position: "absolute",
+                          top: "50%",
+                          right: "10px",
+                          transform: "translateY(-50%)",
+                          cursor: "pointer",
+                      }}
+                      onClick={togglePassword}
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
                   </div>
-                </Form>
-                 {!isTokenValid && (
-                  <div className="text-center text-danger mt-5">
-                    <span>{message}. {" "}<a href={`/reset-password-request`} style={{ color: "#7126B5" }}>
+                </div>
+              </Form.Group>
+
+              {/* Confirm new password form field */}
+              <Form.Group controlId="confirmPassword">
+                <Form.Label>Ulangi Password Baru</Form.Label>
+                <div style={{ position: "relative" }}>
+                  <Form.Control
+                    name='confirmPassword'
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder="Ulangi password baru"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    style={{ 
+                      borderRadius: "16px",
+                      marginTop: "4px" 
+                    }}
+                    required
+                  />
+                  <div
+                      style={{
+                          position: "absolute",
+                          top: "50%",
+                          right: "10px",
+                          transform: "translateY(-50%)",
+                          cursor: "pointer",
+                      }}
+                      onClick={togglePassword}
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </div>
+                </div>
+              </Form.Group>
+              <div className="text-muted mb-4 mt-2">
+                <span>* At least 8 characters long and a number</span>
+              </div>
+
+              {/* Submit button */}
+              <Button
+                type="submit"
+                className="w-100"
+                disabled={isPending}
+                style={{
+                  backgroundColor: isTokenValid ? '#7126B5' : '#bfa0d7',
+                  borderColor: isTokenValid ? '#7126B5' : '#bfa0d7',
+                  borderRadius: "16px",
+                  boxShadow: '4px 4px 10px 2px rgba(0, 0, 0, 0.2)',
+                }}
+              >
+                {isPending ? "Menyimpan..." : "Simpan"}
+              </Button>
+
+              {/* Back to login link */}
+              <div className="text-center mt-3">
+                <span>
+                  Sudah ingat password?{" "}
+                  <Link
+                    to={`/login`}
+                    style={{ color: "#7126B5", fontWeight: "bold" }}
+                  >
+                    Masuk sekarang
+                  </Link>
+                </span>
+              </div>
+
+              {/* Invalid token response message */}
+              {!isTokenValid && (
+                <div className="text-center text-danger mt-5">
+                  <span>
+                    {message || "Token has expired or is invalid"}. {" "}
+                    <Link 
+                      to={`/reset-password-request`} 
+                      style={{ color: "#7126B5" }}>
                       Please try again
-                    </a></span>
-                  </div>
-                )}
-              
-            </div>
-            )}
-          </Container>
+                    </Link>
+                  </span>
+                </div>
+              )}
+              {/* Display error messages */}
+              {message && <p style={{ color: "red", paddingTop: "5px" }}>{message}</p>}
+          </Form>
+        )}
         </Col>
       </Row>
-    </>
+    </section>
   );
 }
