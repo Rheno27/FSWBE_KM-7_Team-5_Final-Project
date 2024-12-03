5967d95b575042b035d51cefe0085d48ce6938f5
Jovina Gustari Juwita <71329253+jovinagustari@users.noreply.github.com>
2024-12-03 17:38:06 +0700

    Feature reset password (#29)
    * add reset-password ui
    
    * adjust reset-password page ui
    
    * add & gitignore .env
    
    * add:reset-password-request
    
    * fix:reset-password
    
    * fix:reset-password
    
    * add:payment
    
    * Revert "add:payment"
    
    This reverts commit 012b15098b490518199decdba9a3fb79a5950098.
    
    * fix: update reset-password
    
    * fix : reset password
    
    * fix : reset password
    
    ---------
    
    Co-authored-by: OneR7 <rhenojulius@gmail.com>

--- AirJourney/src/main.jsx
+++ AirJourney/src/main.jsx
@@ -1,17 +1,19 @@
-import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { Provider } from "react-redux";
 import { store } from './redux/store';
 import { Provider } from 'react-redux'
-import { routeTree } from "./routeTree.gen";
+import { routeTree } from "./routeTree.gen"
+import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // Create the router instance
 const router = createRouter({ routeTree });
+
 // Create a client
 const queryClient = new QueryClient();
+
 // Render the app
 const rootElement = document.getElementById("root");
 if (!rootElement.innerHTML) {
@@ -20,7 +22,9 @@ if (!rootElement.innerHTML) {
         <StrictMode>
             <Provider store={store}>
                 <QueryClientProvider client={queryClient}>
-                    <RouterProvider router={router} />
+                    <BrowserRouter>
+                        <RouterProvider router={router} />
+                    </BrowserRouter>
                 </QueryClientProvider>
             </Provider>
         </StrictMode>
--- AirJourney/src/routes/__root.jsx
+++ AirJourney/src/routes/__root.jsx
@@ -1,22 +1,25 @@
 import * as React from "react";
-import { Outlet, createRootRoute } from "@tanstack/react-router";
-import NavigationBar from "../components/Navbar";
-import { Container } from "react-bootstrap";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import NavigationBar from "../components/Navbar";
 
 export const Route = createRootRoute({
     component: RootComponent,
 });
 
 function RootComponent() {
-    return (
+
+const hideNavbar = ["/register", "/login", "/reset-password", "/reset-password-request", "/otp"];
+const showNavbar = !hideNavbar.includes(window.location.pathname);
+
+    return (    
         <>
-            <NavigationBar />
-            <Container> </Container>
+            {showNavbar && (
+                <NavigationBar />
+            )}
             <Outlet />
-            
             <ToastContainer limit={3} />
             <TanStackRouterDevtools />
         </>
