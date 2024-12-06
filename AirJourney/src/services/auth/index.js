4d3a6efd9845a9ec8a6ff84d1cffb0b2d8c1bb79
Ramzy <144866519+Bamaramzy@users.noreply.github.com>
2024-12-06 22:35:04 +0700

    Feature hasil pencarian (#36)
    * add register page
    
    * integrate register
    
    * add services register
    
    * try pull
    
    * fix register layout
    
    * register maintenance
    
    * fix fetch
    
    * register fix
    
    * integrate register
    
    * remove .env push
    
    * add toast
    
    * fixing detail penerbangan
    
    * fix accordion arrow
    
    * try responsive
    
    ---------
    
    Co-authored-by: chawnima <mferdyalviansyah@gmail.com>

--- AirJourney/src/services/auth/index.js
+++ AirJourney/src/services/auth/index.js
@@ -1,23 +1,22 @@
 import axios from 'axios'
 
 export const login = async (body) => {
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
-        body: JSON.stringify(body),
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-        }
-    });
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
+    body: JSON.stringify(body),
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    const result = await response.json();
+  const result = await response.json();
 
-    if (!response.ok) {
-        throw new Error(result?.message || 'Unknown error');
-    }
+  if (!response.ok) {
+    throw new Error(result?.message || "Unknown error");
+  }
 
-    return result?.data;
+  return result?.data;
 };
-
 
 export const register = async (request) => {
   const formData = new FormData();
